d0e0a9c8446f92eca7a1fff244bbdd4a2ea7e15c
Andrew Omondi <andrew.omondi@microsoft.com>
2020-05-24 03:00:28 +0300

    Cleanup

---
+++ ProjectEuler/Solutions/Problem4.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Euler
+{
+    public class Problem4:Problem
+    {
+        public override string Answer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Libraries/ListTests.cs
@@ -0,0 +1,43 @@
+﻿using Project_Euler;
+using Xunit;
+
+namespace ProjectEulerTests
+{
+    public class ListTests
+    {
+        [Fact]
+        public void CanAddToList()
+        {
+            var list = new List<int>();
+
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+            list.Add(6);
+
+            Assert.Equal(4, list.ToArray().Length);
+            Assert.Equal(1, list.ToArray()[0]);
+            Assert.Equal(3, list.ToArray()[1]);
+            Assert.Equal(5, list.ToArray()[2]);
+            Assert.Equal(6, list.ToArray()[3]);
+        }
+
+        [Fact]
+        public void CanFilterList()
+        {
+            var list = new List<int>();
+
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+            list.Add(6);
+
+            list = list.Where(item => item > 2);
+
+            Assert.Equal(3, list.ToArray().Length);
+            Assert.Equal(3, list.ToArray()[0]);
+            Assert.Equal(5, list.ToArray()[1]);
+            Assert.Equal(6, list.ToArray()[2]);
+        }
+    }
+}
